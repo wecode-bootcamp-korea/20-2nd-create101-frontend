9ae4944bfb525264514249b6c956428815733a81
dosilv <do.silver2@gmail.com>
2021-05-25 13:19:36 +0900

    Add: initial settings

--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -4,5 +4,36 @@ import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    
+    @media screen and (max-width: 415px) {
+      max-width: 100%;
+    }
+  }
+  
+  html {
+    font-size: 18px;
+  }
+  
+  button,
+  input {
+    background-color: transparent;
+    outline: none;
+    border: none;
+    font-size: inherit;
+  }
+
+ul,li {
+  list-style: none;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
   `;
 export default GlobalStyle;
--- src/Styles/theme.js
+++ src/Styles/theme.js
@@ -1,6 +1,7 @@
 const theme = {
   orange: '#FF5704',
-  pink: 'e94650',
+  pink: '#e94650',
+  mobile: 'screen and (max-width: 415px)',
 };
 
 export default theme;
