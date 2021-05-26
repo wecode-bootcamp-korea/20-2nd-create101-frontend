8ec119aa3212d2ec1046b792af47874b9603c6d3
Chris <wychrischo@gmail.com>
2021-05-26 16:18:33 +0900

    멘토님 수정 사항 반영

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import ClassCardList from '../../Components/ClassCard/ClassCardList';
 
 function Main() {
-  return <div></div>;
+  return (
+    <div>
+      <ClassCardList />
+    </div>
+  );
 }
 
 export default Main;
